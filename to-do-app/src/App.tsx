68429cca3eac0e9b6c968e248a8011086b4bd064
Kalani Jayawickrama <kalanioj@gmail.com>
2022-07-15 17:02:11 +0530

    css files are added

--- to-do-app/src/App.tsx
+++ to-do-app/src/App.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
-//import logo from './logo.svg';
-import Main from './components/Main';
-import './App.css';
+import React, { FC, ChangeEvent, useState } from "react";
+import "./App.css";
+import TodoTask from "./components/TodoTask";
+import { ITask } from "./Interfaces";
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task};
+    setTodoList([...todoList, newTask]);
+    setTask("");
+  };
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== taskNameToDelete;
+      })
+    );
+  };
+
   return (
-    <Main />
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Enter task..."
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          
+        </div>
+        <button onClick={addTask}>Add</button>
+      </div>
+      <div className="todoList">
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ to-do-app/src/components/TodoTask.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ITask } from "../Interfaces";
+import { Button } from 'antd';
+
+interface Props {
+  task: ITask;
+  completeTask(taskNameToDelete: string): void;
+}
+
+const TodoTask = ({ task, completeTask }: Props) => {
+  return (
+    <div className="task">
+      <div className="content">
+        <span>{task.taskName}</span>
+      </div>
+      <Button
+        onClick={() => {
+          completeTask(task.taskName);
+        }}
+      >
+        Delete
+      </Button>
+    </div>
+  );
+};
+
+
+export default TodoTask;
